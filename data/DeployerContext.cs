5ef27c48942fb14e4bdf7806a3d4d0dee4a120f1
tom <josephdotson@trollcavedev.com>
2022-06-14 07:01:31 -0400

    actually add the context lol

--- data/DeployerContext.cs
+++ data/DeployerContext.cs
@@ -8,7 +8,11 @@ namespace data;
 public class DeployerContext : DbContext
 {
     public DbSet<ApplicationDTO> Applications { get; set; }
-    
+    public DbSet<ConfigDTO> Configs { get; set; }
+    public DbSet<OrganizationDTO> Organizations { get; set; }
+    public DbSet<PipelineDTO> Pipelines { get; set; }
+    public DbSet<PipelineVersionDTO> PipelineVersions { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseNpgsql("Host=localhost:5432;Database=deployer;Username=postgres;Password=testpwd");
 }
